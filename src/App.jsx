5f7792cc7e5c7d9b6657c73fa3b5274f5c287cfa
Michael Chi <michael.chi1997@gmail.com>
2020-12-12 20:40:02 -0500

    Starter code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AuthForm from './components/AuthForm'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <AuthForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AuthForm.jsx
@@ -0,0 +1,98 @@
+import { useState, Fragment } from "react";
+import { Card, Menu, Form, Button } from "semantic-ui-react";
+
+import "../css/AuthForm.css";
+function AuthForm() {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isSigningUp, setIsSigningUp] = useState(false);
+  const [isLoggingIn, setIsLoggingIn] = useState(false)
+
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const [signupEmail, setSignupEmail] = useState("");
+  const [signupPassword, setSignupPassword] = useState("");
+
+  return (
+    <div className="auth-form-wrapper">
+      <Card className="auth-form-card">
+        <Card.Content>
+          <Card.Header className="auth-form-header">Auth Form</Card.Header>
+          <Menu compact secondary>
+            <Menu.Item
+              name="Login"
+              onClick={() => setIsLogin(true)}
+              active={isLogin}
+            ></Menu.Item>
+            <Menu.Item
+              name="Sign up"
+              onClick={() => setIsLogin(false)}
+              active={!isLogin}
+            ></Menu.Item>
+          </Menu>
+          {isLogin ? (
+            <Fragment>
+              <Form>
+                <Form.Field className="auth-form-fields">
+                  <label className="form-labels">Email</label>
+                  <input
+                    placeholder="Email Address"
+                    name="loginEmail"
+                    type="email"
+                    value={loginEmail || ""}
+                    onChange={(e) => setLoginEmail(e.target.value)}
+                  ></input>
+                </Form.Field>
+                <Form.Field className="auth-form-fields">
+                  <label className="form-labels">Password</label>
+                  <input
+                    placeholder="Password"
+                    name="loginPassword"
+                    type="password"
+                    value={loginPassword || ""}
+                    onChange={(e) => setLoginPassword(e.target.value)}
+                  ></input>
+                </Form.Field>
+                <Button className="auth-form-buttons" color="green">
+                  Login
+                </Button>
+              </Form>
+            </Fragment>
+          ) : (
+            <Form>
+              <Form.Field className="auth-form-fields">
+                <label className="form-labels">Email</label>
+                <input
+                  placeholder="Email Address"
+                  name="signUpEmail"
+                  type="email"
+                  value={signupEmail || ""}
+                  onChange={(e) => setSignupEmail(e.target.value)}
+                ></input>
+              </Form.Field>
+              <Form.Field className="auth-form-fields">
+                <label className="form-labels">Password</label>
+                <input
+                  placeholder="Password"
+                  name="signUpPassword"
+                  type="password"
+                  value={signupPassword || ""}
+                  onChange={(e) => setSignupPassword(e.target.value)}
+                ></input>
+              </Form.Field>
+              <Button
+                loading={isSigningUp}
+                className="auth-form-buttons"
+                color="teal"
+              >
+                Sign up
+              </Button>
+            </Form>
+          )}
+        </Card.Content>
+      </Card>
+    </div>
+  );
+}
+
+export default AuthForm;
